8e25e44def384de53f5252a179446fc8b8e30245
ThallesYVasconcelos <thalles.vasconcelos@ccc.ufcg.edu.br>
2024-10-01 11:28:59 -0300

    fix: atualizado hero da home

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Hero = () => {
   return (
     <section className="hero" id="home">
-      <div className="hero-content">
-        <h1>Inovação e Tecnologia com a Fundação COMP+</h1>
+      <h1>Inovação e Tecnologia com a Fundação COMP+</h1>
+      <div className="hero-text-and-button">
         <p>Explore nossos projetos e faça parte da mudança!</p>
         <a href="#programs" className="cta-button">Explore Nossos Projetos</a>
       </div>
